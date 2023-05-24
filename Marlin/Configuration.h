00a7d6094f1d79c5b72bba28628cf22bd5421324
Tom <tommakesmusic@users.noreply.github.com>
2023-05-24 20:26:53 +0100

    change min z value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1755,7 +1755,7 @@
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -15
 #define Y_MIN_POS 0
-#define Z_MIN_POS -1
+#define Z_MIN_POS -3
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 175
